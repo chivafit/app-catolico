9320b0ed5b6d350246f2d8b806e898f6d5e1099e
analyzA.I <contato@analyzai.app>
2026-09-13 11:05:00 -0300

    Stabilize Explore build

--- src/app/explorar/page.tsx
+++ src/app/explorar/page.tsx
@@ -1,1 +1,1 @@
-import {AppShell} from "@/components/AppShell";import {GlobalExplore} from "@/components/GlobalExplore";import {ExploreSolidarityLink} from "@/components/ExploreSolidarityLink";export default function Explorar(){return <AppShell><GlobalExplore/><ExploreSolidarityLink/></AppShell>}
+import {AppShell} from "@/components/AppShell";import {GlobalExplore} from "@/components/GlobalExplore";export default function Explorar(){return <AppShell><GlobalExplore/></AppShell>}
